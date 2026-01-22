7790e36ec76f0ddcae7769bcae880296042af713
dandnirv7 <dandnirv7@gmail.com>
2026-01-23 01:11:26 +0700

    initial setup

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { appConfig } from './config/app.config';
+import { authConfig } from './config/auth.config';
+import { databaseConfig } from './config/database.config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      load: [appConfig, databaseConfig, authConfig],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/config/database.config.ts
@@ -0,0 +1,3 @@
+export const databaseConfig = () => ({
+  databaseUrl: process.env.DATABASE_URL!,
+});
